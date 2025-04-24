ec4478bc76dd23aa10d5d9a672570cebe82dd0d4
joesharratt1229 <joesharratt1229@gmail.com>
2025-04-24 07:32:02 +0000

    added sass code

--- vectoradd/main.cu
+++ vectoradd/main.cu
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <cuda_bf16.h>
 
 #define BLOCK_SIZE 1024
 
@@ -7,7 +8,8 @@ __host__
 void randomiseInput(scalar_t* h_input, size_t size) {
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-            h_input[i * size + j] = static_cast<scalar_t>(rand()) / RAND_MAX;
+            float random = static_cast<float>(rand()) / RAND_MAX;
+            h_input[i * size + j] = static_cast<scalar_t>(random);
         }
     }
 }
@@ -18,9 +20,25 @@ __host__
 void printMatrix(scalar_t* h_input, size_t size) {
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-            printf("%f ", h_input[i * size + j]);
+            float temp = static_cast<float>(h_input[i * size + j]);
+            printf("%f ", temp);
         }
         printf("\n");
+    }
+}
+
+template<typename scalar_t>
+__global__ void vectorAddKernel32BitLoad(scalar_t* __restrict__ d_A, 
+                                         scalar_t* __restrict__ d_B, 
+                                        scalar_t* __restrict__ d_C, 
+                                         size_t size) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    constexpr size_t size_kernel = sizeof(int4) / sizeof(scalar_t);
+    
+    if ((i * sizeof(scalar_t) + size_kernel - 1)< size) {
+        for (size_t j = 0; j < size_kernel; j++) {
+            d_C[i * size_kernel + j] = d_A[i * size_kernel + j] + d_B[i * size_kernel + j];
+        }
     }
 }
 
@@ -32,11 +50,11 @@ __global__ void vectorAddKernel(scalar_t* __restrict__ d_A,
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     constexpr size_t size_kernel = sizeof(int4) / sizeof(scalar_t);
 
-    int4 Ain = ((int4*)d_A)[i]; //casting pointer to int4 128-bit wide loads
+    int4 Ain = ((int4*)d_A)[i]; //casting pointer to int4 128-bit integer - vectorised load into registers
     int4 Bin = ((int4*)d_B)[i];
     int4 Cout;
 
-    scalar_t* Ax = (scalar_t*)&Ain;
+    scalar_t* Ax = (scalar_t*)&Ain; //casting int4 to scalar_t
     scalar_t* Bx = (scalar_t*)&Bin;
     scalar_t* Cx = (scalar_t*)&Cout;
 
@@ -49,14 +67,20 @@ __global__ void vectorAddKernel(scalar_t* __restrict__ d_A,
         int4* Cx4 = (int4*)d_C; //cast output pointer to int4
         Cx4[i] = Cout; //write back to global memory
     }
-    // ToDo: unaligned boundaries
+    else {
+        for (size_t j = 0; j < size_kernel; j++) {
+            if ((i*size_kernel + j) < size) {
+                d_C[i*size_kernel + j] = d_A[i*size_kernel + j] + d_B[i*size_kernel + j];
+            }
+        }
+    }
 }
 
 template <typename scalar_t>
 __host__ void vectorAdd(scalar_t* h_A, scalar_t* h_B, scalar_t* h_C, size_t size) {
-    float* d_A; 
-    float* d_B;
-    float* d_C;
+    __nv_bfloat16* d_A; 
+    __nv_bfloat16* d_B;
+    __nv_bfloat16* d_C;
 
     //calculate number of blocks
     constexpr size_t elements_per_thread = sizeof(int4) / sizeof(scalar_t);
@@ -71,7 +95,8 @@ __host__ void vectorAdd(scalar_t* h_A, scalar_t* h_B, scalar_t* h_C, size_t size
     cudaMemcpy(d_B, h_B, size * size * sizeof(scalar_t), cudaMemcpyHostToDevice);
     cudaMemset(d_C, 0, size * size * sizeof(scalar_t));
     
-    vectorAddKernel<scalar_t><<<numBlocks, BLOCK_SIZE>>>(d_A, d_B, d_C, size*size);
+    vectorAddKernel<scalar_t><<<numBlocks, BLOCK_SIZE>>>(d_A, d_B, d_C, size*size); //40% reduction in time and 20% increase in memory bandwidth
+    //vectorAddKernel32BitLoad<scalar_t><<<numBlocks, BLOCK_SIZE>>>(d_A, d_B, d_C, size*size);
 
     cudaMemcpy(h_C, d_C, size * size * sizeof(scalar_t), cudaMemcpyDeviceToHost);
 
@@ -81,17 +106,22 @@ __host__ void vectorAdd(scalar_t* h_A, scalar_t* h_B, scalar_t* h_C, size_t size
 }
 
 template <typename scalar_t>
-__host__ void vectorAddCpu(scalar_t* h_A, scalar_t* h_B, scalar_t* h_C, size_t size) {
+__host__ void vectorAddCpu(scalar_t* h_A, scalar_t* h_B, float* h_C, size_t size) {
     for (size_t i = 0; i < size * size; i++) {
-        h_C[i] = h_A[i] + h_B[i];
+        float a_temp = static_cast<float>(h_A[i]);
+        float b_temp = static_cast<float>(h_B[i]);
+        h_C[i] = a_temp + b_temp;
     }
 }
 
 template <typename scalar_t>
-__host__ void verifyResult(scalar_t* h_C, scalar_t* h_C_cpu, size_t size) {
+__host__ void verifyResult(scalar_t* h_C, float* h_C_cpu, size_t size) {
     for (size_t i = 0; i < size * size; i++) {
-        if (h_C[i] != h_C_cpu[i]) {
-            printf("Error at position %zu: %f != %f\n", i, h_C[i], h_C_cpu[i]);
+        float c_temp = static_cast<float>(h_C[i]);
+        float c_cpu_temp = h_C_cpu[i];
+        float epsilon = 1e-2;
+        if (fabs(c_temp - c_cpu_temp) > epsilon) {
+            printf("Error at position %zu: %f != %f\n", i, c_temp, c_cpu_temp);
         }
     }
 }
@@ -101,30 +131,30 @@ int main(int argc, char** argv) {
     int DEBUG = atoi(argv[2]);
 
     // Allocate host memory
-    float* h_A = (float*)malloc(shape * shape * sizeof(float));
-    float* h_B = (float*)malloc(shape * shape * sizeof(float));
-    float* h_C = (float*)malloc(shape * shape * sizeof(float));
+    __nv_bfloat16* h_A = (__nv_bfloat16*)malloc(shape * shape * sizeof(__nv_bfloat16)); //using bfloat16 furhter reduces memory usage 10us - 7us
+    __nv_bfloat16* h_B = (__nv_bfloat16*)malloc(shape * shape * sizeof(__nv_bfloat16));
+    __nv_bfloat16* h_C = (__nv_bfloat16*)malloc(shape * shape * sizeof(__nv_bfloat16));
     float* h_C_cpu = (float*)malloc(shape * shape * sizeof(float));
 
     // Randomise input
-    randomiseInput<float>(h_A, shape);
-    randomiseInput<float>(h_B, shape);
+    randomiseInput<__nv_bfloat16>(h_A, shape);
+    randomiseInput<__nv_bfloat16>(h_B, shape);
     if (DEBUG) {
         printf("Matrix A:\n");
-        printMatrix<float>(h_A, shape);
+        printMatrix<__nv_bfloat16>(h_A, shape);
         printf("Matrix B:\n");
-        printMatrix<float>(h_B, shape);
+        printMatrix<__nv_bfloat16>(h_B, shape);
     }
 
     // Vector add
-    vectorAdd<float>(h_A, h_B, h_C, shape);
+    vectorAdd<__nv_bfloat16>(h_A, h_B, h_C, shape);
 
     if (DEBUG) {
         printf("Matrix C:\n");
-        printMatrix<float>(h_C, shape);
+        printMatrix<__nv_bfloat16>(h_C, shape);
     }
 
-    vectorAddCpu<float>(h_A, h_B, h_C_cpu, shape);
+    vectorAddCpu<__nv_bfloat16>(h_A, h_B, h_C_cpu, shape);
 
-    verifyResult<float>(h_C, h_C_cpu, shape);
+    verifyResult<__nv_bfloat16>(h_C, h_C_cpu, shape);
 }
